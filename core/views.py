2bcd9c14c8503a938b99745e8485069cca3d3486
kibet <kibetlelgo49@gmail.com>
2025-04-05 09:06:21 +0300

    signup page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignUpForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
+    
     return render(request, 'core/index.html',{
         'categories':categories,
         'items':items,
@@ -11,3 +13,17 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    form = SignUpForm()
+
+    return render(request, 'core/signup.html', {
+        'form':form
+    })
