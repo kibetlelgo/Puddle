da2fbdcafbb84ba98ad7e968145216acfe57004e
kibet <kibetlelgo49@gmail.com>
2025-04-07 08:56:42 +0300

    Adding items (new item function)

--- item/migrations/0002_alter_category_options_item.py
+++ item/migrations/0002_alter_category_options_item.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
                 ('image', models.ImageField(blank=True, null=True, upload_to='item_images')),
                 ('is_sold', models.BooleanField(default=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('Category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='item.category')),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='item.category')),
                 ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to=settings.AUTH_USER_MODEL)),
             ],
         ),
